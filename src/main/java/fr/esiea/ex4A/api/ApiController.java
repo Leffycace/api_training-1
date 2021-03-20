69e0ae66a3c4888ad7ccb5bd4b4fc8a14a2a70e5
ocruze <33525693+ocruze@users.noreply.github.com>
2021-03-20 17:47:49 +0100

    get matches with fake data

--- src/main/java/fr/esiea/ex4A/api/ApiController.java
+++ src/main/java/fr/esiea/ex4A/api/ApiController.java
@@ -1,13 +1,14 @@
 package fr.esiea.ex4A.api;
 
+import fr.esiea.ex4A.data.Match;
 import fr.esiea.ex4A.data.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -23,5 +24,30 @@ public class ApiController {
         User user = new User(request.get("userEmail"), request.get("userName"), request.get("userTweeter"), request.get("userCountry"), request.get("userSex"), request.get("userSexPref"));
         users.add(user);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/matches")
+    public ResponseEntity<?> getMatches(@RequestParam("userName") String userName, @RequestParam("userCountry") String userCountry) {
+        User requestUser = null;
+
+        for (User user : users) {
+            if (user.getUserName().equalsIgnoreCase(userName) && user.getUserCountry().equalsIgnoreCase(userCountry)) {
+                requestUser = user;
+                break;
+            }
+        }
+
+        // user not found
+        if (requestUser == null) {
+            return new ResponseEntity<>(new ArrayList<User>(), HttpStatus.OK);
+        }
+
+        // find matches for user
+        List<Match> matches = Arrays.asList(
+            new Match("Michel", "michelbgdu77"),
+            new Match("Amandine", "amandinedu36")
+        );
+
+        return new ResponseEntity<>(matches, HttpStatus.OK);
     }
 }
