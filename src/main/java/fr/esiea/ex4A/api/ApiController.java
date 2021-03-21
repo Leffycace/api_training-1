c6eef7c1cb18ee645f9751f86569f3a5cc4581d1
ocruze <33525693+ocruze@users.noreply.github.com>
2021-03-21 22:11:24 +0100

    agify service implementation for create user

--- src/main/java/fr/esiea/ex4A/api/ApiController.java
+++ src/main/java/fr/esiea/ex4A/api/ApiController.java
@@ -1,11 +1,13 @@
 package fr.esiea.ex4A.api;
 
+import fr.esiea.ex4A.agify.AgifyService;
 import fr.esiea.ex4A.data.Match;
 import fr.esiea.ex4A.data.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,9 +21,19 @@ public class ApiController {
      */
     private final ArrayList<User> users = new ArrayList<>();
 
+    private final AgifyService agifyService;
+
+    public ApiController(AgifyService agifyService) {
+        this.agifyService = agifyService;
+    }
+
     @PostMapping("/api/inscription")
-    public ResponseEntity<?> addUser(@RequestBody Map<String, String> request) {
-        User user = new User(request.get("userEmail"), request.get("userName"), request.get("userTweeter"), request.get("userCountry"), request.get("userSex"), request.get("userSexPref"));
+    public ResponseEntity<?> addUser(@RequestBody Map<String, String> request) throws IOException {
+        String userName = request.get("userName");
+        String userCountry = request.get("userCountry");
+        Integer userAge = agifyService.getAge(userName, userCountry);
+
+        User user = new User(request.get("userEmail"), userName, request.get("userTweeter"), userCountry, request.get("userSex"), request.get("userSexPref"), userAge);
         users.add(user);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
