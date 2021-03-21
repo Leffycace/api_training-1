6508f6a6ac184f623c2655caaf403f61eda80cae
ocruze <33525693+ocruze@users.noreply.github.com>
2021-03-21 20:38:48 +0100

    add agify service

---
+++ src/main/java/fr/esiea/ex4A/agify/AgifyService.java
@@ -0,0 +1,25 @@
+package fr.esiea.ex4A.agify;
+
+import org.springframework.stereotype.Service;
+import retrofit2.Call;
+import retrofit2.Response;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class AgifyService {
+
+    private final AgifyClient agifyClient;
+
+    public AgifyService(AgifyClient agifyClient) {
+        this.agifyClient = agifyClient;
+    }
+
+    public Integer getAge(String name, String countryId) throws IOException {
+        Call<Map<String, String>> apiCall = agifyClient.getAge(name, countryId);
+
+        Response<Map<String, String>> response = apiCall.execute();
+        return Integer.parseInt(response.body().getOrDefault("age", "18"));
+    }
+}
